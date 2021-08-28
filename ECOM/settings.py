db7e524af90e9e6866f3e39d5310c629608b9d91
VipulKumar <vipulkumar8292@gmail.com>
2021-08-28 08:40:09 +0530

    database changed

--- ECOM/settings.py
+++ ECOM/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'ECOM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
