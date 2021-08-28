f60965fa45f33bb03d3b180a892b3002912ed986
Vipul Kumar <53464204+vipulkumar8292@users.noreply.github.com>
2021-08-28 11:47:38 +0530

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -10,13 +10,22 @@ from .middlewares.auth import auth_middleware
 
 urlpatterns = [
     path('', Index.as_view(), name='homepage'),
+    
     path('store', store, name='store'),
-
+    path('signup', Signup.as_view(), name='signup'),
+    path('login', Login.as_view(), name='login'),
+    path('logout', logout, name='logout'),
+    path('cart', auth_middleware(Cart.as_view()), name='cart'),
+    path('check-out', CheckOut.as_view(), name='checkout'),
+    path('orders', auth_middleware(OrderView.as_view()), name='orders'), #slash removed
+    path('cart/login', Login.as_view(), name='login'),
+    
+    path('store/', store, name='store'),
     path('signup/', Signup.as_view(), name='signup'),
     path('login/', Login.as_view(), name='login'),
     path('logout/', logout, name='logout'),
     path('cart/', auth_middleware(Cart.as_view()), name='cart'),
-    path('check-out', CheckOut.as_view(), name='checkout'),
-    path('orders', auth_middleware(OrderView.as_view()), name='orders'), #slash removed
-    path('cart/login/', Login.as_view(), name='login')
+    path('check-out/', CheckOut.as_view(), name='checkout'),
+    path('orders/', auth_middleware(OrderView.as_view()), name='orders'), #slash removed
+    path('cart/login/', Login.as_view(), name='login'),
 ]
